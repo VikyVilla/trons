a589cd2360b92a715aadf376388d31684b2ce147
vikyvilla <vigeshvilha@vajro.com>
2021-01-16 15:12:39 +0530

    added try catch

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,16 +18,20 @@ router.get('/get_records', async(req, res) => {
     const folder_name = req.query.folder_name;
     const sort = req.query.sort
 
-    if (folder_name) {
-        // get a particular folder with childs
-        let read_records = await readData(DB_NAME, DB_COLLECTION_NAME, sort, '', '', { path: { '$regex': '.*' + folder_name + '.*' } })
-        let result = treeify(read_records)
-        return res.send(result)
-    } else {
-        let read_records = await readData(DB_NAME, DB_COLLECTION_NAME, sort, '', '', '')
-        console.log("read_records", read_records)
-        let result = treeify(read_records)
-        return res.send(result)
+    try {
+        if (folder_name) {
+            // get a particular folder with childs
+            let read_records = await readData(DB_NAME, DB_COLLECTION_NAME, sort, '', '', { path: { '$regex': '.*' + folder_name + '.*' } })
+            let result = treeify(read_records)
+            return res.send(result)
+        } else {
+            let read_records = await readData(DB_NAME, DB_COLLECTION_NAME, sort, '', '', '')
+            console.log("read_records", read_records)
+            let result = treeify(read_records)
+            return res.send(result)
+        }
+    } catch (err) {
+        return res.sendStatus(400)
     }
 
     function treeify(files) {
@@ -63,17 +67,21 @@ router.get('/get_records', async(req, res) => {
 
 router.post('/create_records', async(req, res) => {
     const body = req.body;
+    try {
+        req.body.map(element => {
+            if (element.path === '/') {
+                return res.sendStatus(400)
+            }
+        });
 
-    req.body.map(element => {
-        if (element.path === '/') {
-            return res.sendStatus(400)
-        }
-    });
+        // need to add some strong validation for body
+        // check the path already exists
 
-    // need to add some strong validation for body
-
-    let create_records = await createBulkData(DB_NAME, DB_COLLECTION_NAME, body)
-    return res.send(create_records.ops)
+        let create_records = await createBulkData(DB_NAME, DB_COLLECTION_NAME, body)
+        return res.send(create_records.ops)
+    } catch (err) {
+        return res.sendStatus(400)
+    }
 
 })
 
@@ -81,13 +89,16 @@ router.delete('/delete_records', async(req, res) => {
     let body = [];
 
     // need to add some strong validation for body
+    try {
+        req.body.map(element => {
+            body.push(new ObjectID(element._id))
+        });
 
-    req.body.map(element => {
-        body.push(new ObjectID(element._id))
-    });
-
-    let delete_records = await deleteBulkData(DB_NAME, DB_COLLECTION_NAME, body)
-    return res.send(delete_records)
+        let delete_records = await deleteBulkData(DB_NAME, DB_COLLECTION_NAME, body)
+        return res.send(delete_records)
+    } catch (err) {
+        return res.sendStatus(400)
+    }
 
 })
 
@@ -100,41 +111,44 @@ router.put('/put_records', async(req, res) => {
     if (source_data.path === "/") {
         return res.sendStatus(400)
     }
+    try {
+        if (has(source_data, 'type') && source_data.type === 'image') {
+            const split = source_data.path.split("/") //[ "", "a" ]
+            const file_name = split[split.length - 1] // a
+            const _id = source_data._id
 
-    if (has(source_data, 'type') && source_data.type === 'image') {
-        const split = source_data.path.split("/") //[ "", "a" ]
-        const file_name = split[split.length - 1] // a
-        const _id = source_data._id
-
-        if (destination_data.path === '/') {
-            source_data.path = destination_data.path + file_name
-        } else {
-            source_data.path = destination_data.path + '/' + file_name
-        }
-        delete source_data._id
-
-        let update_record = await updateData(DB_NAME, DB_COLLECTION_NAME, { _id }, source_data)
-        return res.send(update_record)
-    } else if (has(source_data, 'type') && source_data.type === 'directory') {
-
-        let read_records = await readData(DB_NAME, DB_COLLECTION_NAME, '', '', '', { path: { '$regex': '.*' + source_data.path + '.*' } })
-        let map_records = [],
-            ids = [];
-
-        read_records.map(data => {
-            ids.push(new ObjectID(data._id))
-            delete data._id
             if (destination_data.path === '/') {
-                data.path = destination_data.path + source_data.name + data.path.replace(source_data.path, "")
+                source_data.path = destination_data.path + file_name
             } else {
-                data.path = destination_data.path + '/' + source_data.name + data.path.replace(source_data.path, "")
+                source_data.path = destination_data.path + '/' + file_name
             }
-            map_records.push({...data })
-        })
+            delete source_data._id
 
-        Promise.all([deleteBulkData(DB_NAME, DB_COLLECTION_NAME, ids), createBulkData(DB_NAME, DB_COLLECTION_NAME, map_records)]).then((values) => {
-            return res.send(values[1].ops)
-        })
+            let update_record = await updateData(DB_NAME, DB_COLLECTION_NAME, { _id }, source_data)
+            return res.send(update_record)
+        } else if (has(source_data, 'type') && source_data.type === 'directory') {
+
+            let read_records = await readData(DB_NAME, DB_COLLECTION_NAME, '', '', '', { path: { '$regex': '.*' + source_data.path + '.*' } })
+            let map_records = [],
+                ids = [];
+
+            read_records.map(data => {
+                ids.push(new ObjectID(data._id))
+                delete data._id
+                if (destination_data.path === '/') {
+                    data.path = destination_data.path + source_data.name + data.path.replace(source_data.path, "")
+                } else {
+                    data.path = destination_data.path + '/' + source_data.name + data.path.replace(source_data.path, "")
+                }
+                map_records.push({...data })
+            })
+
+            Promise.all([deleteBulkData(DB_NAME, DB_COLLECTION_NAME, ids), createBulkData(DB_NAME, DB_COLLECTION_NAME, map_records)]).then((values) => {
+                return res.send(values[1].ops)
+            })
+        }
+    } catch (err) {
+        return res.sendStatus(400)
     }
 
 })
